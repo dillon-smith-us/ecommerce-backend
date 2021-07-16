3025f15c8734590d91b9f358b71e8f398143676b
Dillon Smith <dillsmth@gmail.com>
2021-07-15 20:12:58 -0500

    initial commits added

---
+++ routes/api/category-routes.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  Category.findAll({
+    include: {
+      model: Product,
+        // be sure to include its associated Products
+      attributes: ['product_name']
+    }
+  })
+  .then(dbCatData => res.json(dbCatData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  Category.findOne({
+    where: {
+      id: req.params.id
+    }, 
+    // be sure to include its associated Products
+    include: {
+      model: Product,
+      attributes: ['category_id']
+    }
+  })
+  .then(dbCatData => res.json(dbCatData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
