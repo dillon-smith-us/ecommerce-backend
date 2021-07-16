350f445ba4d38dc30957f8fb72707c87c886ee9a
Dillon Smith <dillsmth@gmail.com>
2021-07-16 01:28:03 -0500

    fixing async function

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -135,8 +135,8 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  
+router.delete('/:id', async (req, res) => {
+
   // delete one product by its `id` value
 
   try {
